197e9c1545a02157dbcf7cabff9fce579a4dfb87
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-05-20 13:45:59 +0300

    adds pnotify, removes duplications

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,16 @@
+import countryTemplate from './countryTemplate.hbs'
+import countryList from './countryList.hbs'
+import { error, defaults } from '@pnotify/core'
+import '@pnotify/core/dist/BrightTheme.css'
+
+defaults.sticker = false
+defaults.closer = false
+defaults.icon = false
+defaults.minHeight = '20px'
+defaults.delay = 4000
+
 const axios = require('axios')
 const countryCardEl = document.querySelector('.country-card')
-
-import countryTemplate from './countryTemplate.hbs'
 
 axios.defaults.baseURL = 'https://restcountries.eu/rest/v2/name'
 
@@ -9,9 +18,18 @@ export default function fetchCountries(searchQuery) {
   return axios
     .get(`/${searchQuery}?fields=name;capital;population;languages;flag`)
     .then((response) => {
-      console.log(response.data.[0])
-      const markup = countryTemplate(response.data.[0]);
-      countryCardEl.insertAdjacentHTML('afterbegin', markup)
+      // console.log(response.data)
+      if (response.data.length > 10) {
+        return error({
+          text: 'Too many matches found. Please enter a more specific query!',
+        })
+      } else if (response.data.length >= 2 && response.data.length <= 10) {
+        const markup = countryList(response.data)
+        countryCardEl.insertAdjacentHTML('afterbegin', markup)
+      } else {
+        const markup = countryTemplate(response.data[0])
+        countryCardEl.insertAdjacentHTML('afterbegin', markup)
+      }
     })
     .catch((error) => console.log(error))
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ const inputEl = document.querySelector('.country')
 inputEl.addEventListener('input', _.debounce(onInput, 500))
 
 function onInput(e) {
+  const countryTpl = document.querySelector('.country-card-tpl')
+  const countryList = document.querySelector('.country-list')
+  if (countryTpl) {
+    countryTpl.remove()
+  }
+  if (countryList) {
+    countryList.remove()
+  }
   const inputText = e.target.value
   fetchCountries(inputText)
 }
